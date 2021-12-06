b8894a75ce56161b2e71ce8ade90655ed874e9a9
kimhyerin0909 <khyerin0909@gmail.com>
2021-12-06 21:51:49 +0900

    정적 컨텐츠, MVC, API 공부 실습

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import jdk.jfr.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +14,17 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "spring!"); //hello.html에 data변수이름으로 "hello!"를 넘김
         return "hello"; //resource/templates에 hello를 찾아 렌더링함 (Thymelead 템플릿 엔진 처리)
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
     }
 }
