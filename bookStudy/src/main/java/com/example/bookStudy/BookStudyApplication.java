cf04e4038c916d46352b4cd64d199d8a347f250d
kimhyerin0909 <khyerin0909@gmail.com>
2021-12-09 20:19:33 +0900

    GetMapping, RestController 이해

--- bookStudy/src/main/java/com/example/bookStudy/BookStudyApplication.java
+++ bookStudy/src/main/java/com/example/bookStudy/BookStudyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //@Controller, @ResponseBody를 합쳐놓은 어노테이션 -> html 파일이 없어도 웹 브라우저 실행 가능
 @SpringBootApplication
 public class BookStudyApplication {
 
